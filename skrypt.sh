532e28fd595a6984a2579455c37141848a73a7b9
Jakub <117046128+KLesiu@users.noreply.github.com>
2024-04-21 13:27:49 +0200

    custom logs

--- skrypt.sh
+++ skrypt.sh
@@ -7,13 +7,29 @@ else
 fi
 
 if [ "$1" == "--logs" ]; then
-    for ((i=1; i<=100; i++)); do
+    if [ -z "$2" ]; then
+        for ((i=1; i<=100; i++)); do
+            filename="log${i}.txt"
+            echo "Nazwa pliku: $filename" > "$filename"
+            echo "Skrypt: $0" >> "$filename"
+            echo "Data utworzenia: $(date)" >> "$filename"
+        done
+        echo "Utworzono 100 plików z logami."
+        exit 1
+    fi
+
+    num_files="$2"
+    
+    if ! [[ "$num_files" =~ ^[0-9]+$ ]]; then
+        echo "Podana wartość nie jest liczbą całkowitą dodatnią."
+        exit 1
+    fi
+    
+    for ((i=1; i<=$num_files; i++)); do
         filename="log${i}.txt"
         echo "Nazwa pliku: $filename" > "$filename"
         echo "Skrypt: $0" >> "$filename"
         echo "Data utworzenia: $(date)" >> "$filename"
     done
-    echo "Utworzono 100 plików z logami."
-else
-    echo "Użycie: ./skrypt.sh --logs"
+    echo "Utworzono $num_files plików z logami."
 fi
